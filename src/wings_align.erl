eebed66b37da93a8a3b5b702e412fb8cea6d9885
Iban Oyharcabal <iban.oyharcabal@u-bordeaux.fr>
2024-02-12 14:38:21 +0100

    init Unitize

--- src/wings_align.erl
+++ src/wings_align.erl
@@ -228,9 +228,10 @@ unitize_obj(We) ->
     [Min,Max] = wings_vertex:bounding_box(We),
     Size = e3d_vec:sub(Max, Min),
     Center = e3d_vec:average(Min, Max),
-    Scale = 2.0 / e3d_vec:len(Size),
+     Scale = 2.0 / e3d_vec:len(Size),
+    Scale =  e3d_vec:len({2.0,2.0,2.0}) / e3d_vec:len(Size),
     LocMat = e3d_mat:translate(e3d_vec:neg(Center)),
-    SizMat = e3d_mat:scale(Scale),
+    SizMat = e3d_mat:scale(Scale), 
     Matrix = e3d_mat:mul(SizMat, LocMat),
     wings_we:transform_vs(Matrix, We).
 
