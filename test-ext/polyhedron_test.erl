50c951a7f7ad9c290d63b222b72bcc0a169fc48f
Corentin Seutin <corentin.seutin@outlook.fr>
2024-03-10 17:03:01 +0100

    Tests pour la fonction wpc_polyhedron:compare_min_max() écrits (3); La compilation du fichier test fonctionne contrairement à l'exécution qui n'arrive pas à trouver la fonction compare_min_max() du module wpc_polyhedron (certainement parce que le fichier wpc_polyhedron.erl n'est pas compilé)

--- test-ext/polyhedron_test.erl
+++ test-ext/polyhedron_test.erl
@@ -1,15 +1,48 @@
 -module(polyhedron_test).
 
--compile(export_all).
+%-compile({parse_transform, tools}).
+-include_lib("eunit/include/eunit.hrl").
+%-include_lib("../src/wings.hrl").
+-export([go/0]).
+-import(wpc_polyhedron, [compare_min_max/2]).
 
--include_lib("wings/src/wings.hrl").
-
-go()  -> start().
+go() -> start().
 
 start() ->
-    Res = run_test_compare_min_max(),
-    ok.
+    ?assert(
+        compare_min_max_test()
+    ).
 
-run_test_compare_min_max() ->
-    true.
+compare_min_max_test() ->
+    ?assert(
+        ( 
+            compare_min_max_one_test() andalso
+            compare_min_max_two_test() andalso
+            compare_min_max_three_test() 
+        ) =:= ok
+    ).
 
+%%%%%%%%%%%%%%%%%%%compare_min_max tests%%%%%%%%%%%%%%%%%%%
+%Max should be replaced
+compare_min_max_one_test() -> 
+    ?assert(
+        wpc_polyhedron:compare_min_max({1,2,3},[{0,0,0},{0,0,0}]) =:= 
+        [{0,0,0},{1,2,3}]
+    ).
+
+%Min should be replaced
+compare_min_max_two_test() -> 
+    ?assert(
+        wpc_polyhedron:compare_min_max({1,2,3},[{4,4,4},{4,4,4}]) =:= 
+        [{1,2,3},{4,4,4}]
+    ).
+
+%Min and Max should not be replaced
+compare_min_max_three_test() -> 
+    ?assert(
+        wpc_polyhedron:compare_min_max({0.5,0.5,0.5},[{0,0,0},{1,1,1}]) =:= 
+        [{0,0,0},{1,1,1}]
+    ).
+
+%Compilation line : 
+    %erl -pa ../plugins_src/primitives/
